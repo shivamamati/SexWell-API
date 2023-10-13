0c053ed56e83369942a553be362a5ff669c2f2d8
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2023-10-13 14:27:31 -0400

    feat: basic express server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers } from "./resolvers.js";
 import { typeDefs } from "./typesDefs.js";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import pkg from "body-parser";
+const { json } = pkg;
+import express from "express";
+
+const app = express();
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
@@ -14,8 +21,13 @@ const server = new ApolloServer({
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+await server.start();
+app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware(server),
+);
+app.listen(8080, () => {
+    console.log("sever starting on port 8080");
 });
-
-console.log(`🚀  Server ready at: ${url}`);
